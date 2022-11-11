f51dfa43c61118630f782b27122b49dc19d78f33
stopshien <stopforxcode@gmail.com>
2022-11-11 11:39:51 +0800

    Initial Commit

---
+++ firebasePractice(UIKit)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  firebasePractice(UIKit)
+//
+//  Created by 沈庭鋒 on 2022/11/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
